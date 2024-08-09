6661c0988e5f8eef9c1e86b5e63184c059a7760b
Stefan Lenders <stefan@lenders-it.nl>
2024-08-09 16:22:53 +0200

    Keep breaking stuff

--- drivers/adguard-home/driver.js
+++ drivers/adguard-home/driver.js
@@ -1,58 +1,52 @@
 'use strict';
 
-const Driver = require('../../lib/Driver');
+const { Driver } = require('homey');
 const AdguardAPI = require('../../lib/AdguardAPI');
 
 let foundDevices = [];
 
 class AdguardHomeDriver extends Driver {
 
-  async _onPairSearchDevices(data) {
-    this.log('_onPairSearchDevices');
+  onPair(session) {
+    session.setHandler('search_devices', async (data) => {
+      foundDevices = []
 
-    foundDevices = []
-
-    this.log(data)
-
-    const api = new AdguardAPI(data);
-
-    await api.httpRequest('/control/status')
-      .then(result => {
-        this.log(result)
-        if (result) {
-          this.log("Can connect to AdGuard Home")
-          foundDevices = [
-            {
-              name: "Adguard Home test",
-              data: {
-                id: data.address
-              },
-              store: {
-                address: data.address
-              },
-              settings: {
-                address: data.address,
-                username: data.username,
-                password: data.password
-              }
-            }];
-        } else {
-          this.log('No Result');
-          console.error('processData', data);
-          throw new Error("Error processing API data");
-        }
-      }).catch(error => {
-        return error;
-      });
-
-    return true
-  }
-
-  async _onPairListDevices() {
-    this.log('_onPairListDevices');
-    this.log(foundDevices);
-
-    return foundDevices;
+      this.log(data)
+  
+      const api = new AdguardAPI(data);
+  
+      await api.httpRequest('/control/status')
+        .then(result => {
+          this.log(result)
+          if (result) {
+            this.log("Can connect to AdGuard Home")
+            foundDevices = [
+              {
+                name: "Adguard Home test",
+                data: {
+                  id: data.address
+                },
+                store: {
+                  address: data.address
+                },
+                settings: {
+                  address: data.address,
+                  username: data.username,
+                  password: data.password
+                }
+              }];
+          } else {
+            this.log('No Result');
+            console.error('processData', data);
+            throw new Error("Error processing API data");
+          }
+        }).catch(error => {
+          return error;
+        });
+  
+      this.log("Finished ")
+      return true
+    });
   }
 }
 
